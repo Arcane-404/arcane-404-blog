a74d1cc41ce7c3794968fca78185e787094e46c1
Michael F. Alvarez <37319530+awwmicky@users.noreply.github.com>
2021-09-25 14:34:26 -0700

    update old code w/ new code

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,3 @@
-// export * as UserControllers from './_User.controllers.js'
+export * as UserControllers from './_User.controllers.js'
 export * as BlogControllers from './_Blog.controllers.js'
 export * as VoteControllers from './_Vote.controllers.js'
