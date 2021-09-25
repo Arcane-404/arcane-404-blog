4a934abdf148e8173f0174a536e200e44b62d45d
Michael F. Alvarez <37319530+awwmicky@users.noreply.github.com>
2021-09-25 14:31:00 -0700

    update old code with new code

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import logger from 'morgan'
 import cors from 'cors'
+
 import {
 	userRoutes, blogRoutes, voteRoutes
 } from './routes/index.js'
 
 const app = express()
+
+
+import auth from './middlewares/auth.js'
+import authAdmin from './middlewares/auth_admin.js'
 
 // middleware setup
 app.use(logger('combined'))
@@ -13,14 +18,15 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// EDIT: figure out path setup
 app.use('/api/users', userRoutes)
 app.use('/api/blog', blogRoutes)
 app.use('/api/vote', voteRoutes)
 
-// EDIT: can we get the '/' route ready for react
-app.get('/', (req, res) => {
-	res.send('index page')
-})
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'))
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
+}
 
 export default app
