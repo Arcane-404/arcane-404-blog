27b2de61e3ef1b0a3f6022de51087feb2602eb70
hermsicle <hermanliu95@gmail.com>
2021-09-25 12:01:58 -0700

    update code, remove old code

--- controllers/_Blog.controllers.js
+++ controllers/_Blog.controllers.js
@@ -1,33 +1,27 @@
-// Modularizing
-import BlogModel from '../model/_Blog.model.js'
+import Blog from '../model/_Blog.model.js'
 
-export const findAll = (req, res) => {
-	BlogModel.find().then((blogs) => {
-		res.json(blogs)
-	})
+export const createBlog = async (req, res) => {
+	try {
+		// getting title, body from front end
+		const { title, body } = req.body
+		const id = req.auth.id
+
+		// Create a new blog
+		const newBlog = await Blog.create({
+			author: {
+				id
+			},
+			title,
+			body
+		})
+
+		// return newBlog to client
+		return res.status(200).json({ blog: newBlog })
+
+	} catch (err) {
+		console.log(err.message)
+		// if error return err message to clinet
+		return res.status(500).json({ message: err.message })
+	}
 }
 
-export const postOne = (req, res) => {
-	BlogModel.create({
-		title: req.body.title,
-		body: req.body.body
-	}).then((newBlog) => {
-		res.json(newBlog)
-		// console.log(newBlog)
-	}).catch((err) => {
-		res.json(err)
-	})
-}
-
-export const updateOne = (req, res) => {
-	BlogModel.findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true })
-		.then((updated) => {
-			res.json(updated)
-		}).catch((err) => res.send(err))
-}
-
-export const deleteOne = (req, res) => {
-	BlogModel.findByIdAndDelete({ _id: req.params.id }).then(() => {
-		res.send(`deleted ${req.params.id}`)
-	}).catch((err) => res.send(err))
-}
--- routes/_Blog.routes.js
+++ routes/_Blog.routes.js
@@ -5,9 +5,6 @@ import { BlogControllers } from '../controllers/index.js'
 const blogRouter = express.Router()
 
 blogRouter
-	.get('/', BlogControllers.findAll)
-	.post('/create', BlogControllers.postOne)
-	.put('/blog/:id', BlogControllers.updateOne)
-	.delete('/blog/:id', BlogControllers.deleteOne)
+	.post('/create', BlogControllers.createBlog)
 
 export default blogRouter
